617aa53786dfa1ee41c64145ac5dd58493eb6050
kaposen144 <58549054+kaposen144@users.noreply.github.com>
2019-12-11 21:36:10 +0300

    1

--- src/demo.js
+++ src/demo.js
@@ -15,22 +15,25 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
   card: {
     display: "flex",
     maxWidth: 600,
     paddingLeft: 15,
-    paddingRight: 15
+    paddingRight: 15,
+    paddingTop: 10
   },
   avatar: {
     height: 49,
     width: 49,
-    marginRight: 5,
-    marginLeft: 5,
-    marginTop: 15
+    marginRight: 5
   },
-  content: {},
+  content: {
+    marginLeft: 5,
+    padding: 0
+  },
   bottom: {
     justifyContent: "space-between"
   }
@@ -46,7 +49,11 @@ export default function RecipeReviewCard() {
 
   return (
     <Card className={classes.card}>
-      <Avatar aria-label="recipe" className={classes.avatar}>
+      <Avatar
+        aria-label="recipe"
+        className={classes.avatar}
+        src="https://pbs.twimg.com/profile_images/1175295104912961537/TO5Wcyda_reasonably_small.jpg"
+      >
         R
       </Avatar>
       <div className={classes.content}>
@@ -56,31 +63,31 @@ export default function RecipeReviewCard() {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Oxxxymiron
-          @norimyxxxo
-          ·
-          Dec 4
-          "
-          style={{ paddingBottom: 0 }}
+          title={
+            <Typography variant="subtitle2">
+              Oxxxymiron @norimyxxxo · Nov 6
+            </Typography>
+          }
+          className={classes.content}
           subheader={
-            <Typography color="textSecondary" component="p">
-              This impressive paella is a perfect party dish and a fun meal to
-              cook together with your guests. Add 1 cup of frozen peas along
-              with the mussels, if you like.
+            <Typography variant="body2">
+              Наконец-то на самом заседании, а не в зале трансляции. Семья Егора
+              провела в зал как брата ^_^, приставы все поняли и на этот раз
+              отреагировали адекватно.
             </Typography>
           }
         />
         <CardActions className={classes.bottom}>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="show-more">
+          <Button aria-label="add to favorites">
+            <FavoriteIcon />2
+          </Button>
+          <Button aria-label="show-more">
+            <ShareIcon /> 3
+          </Button>
+          <Button aria-label="show-more">
             <ShareIcon />
-          </IconButton>
-          <IconButton aria-label="show-more">
-            <ShareIcon />
-          </IconButton>
-          <IconButton
+          </Button>
+          <Button
             className={clsx(classes.expand, {
               [classes.expandOpen]: expanded
             })}
@@ -89,7 +96,7 @@ export default function RecipeReviewCard() {
             aria-label="show more"
           >
             <ExpandMoreIcon />
-          </IconButton>
+          </Button>
         </CardActions>
       </div>
     </Card>
